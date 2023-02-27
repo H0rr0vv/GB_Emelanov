581e68f3bd2c64c187aef7b647bbe8e28896141e
Horrow <faik2k@gmail.com>
2023-02-27 21:36:12 +1000

    Решение задачи 12

---
+++ Tasks/Task 12/Program.cs
@@ -0,0 +1,32 @@
+﻿// 
+
+
+Console.WriteLine("Введите большее число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для проверки");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int CratDigit(int numA, int numB) //
+{
+    int result = numberA % numberB;
+    return result;
+}
+
+void PrintMod(int remain)
+{
+    if (remain == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine($"Не кратно, остаток {remain}");
+    }
+}
+
+int cratDigit = CratDigit(numberA, numberB);
+
+PrintMod(cratDigit);
+
+
+
