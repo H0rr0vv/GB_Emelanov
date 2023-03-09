fea1ee4719aa4980a0c7cc4a64e0f601e339004e
Horrow <faik2k@gmail.com>
2023-03-09 19:54:42 +1000

    task 29

---
+++ Tasks/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 27: Напишите программу, 
+//которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число, сумму цифр которого надо посчитать");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int summOfNumber = SummOfNumber (number);
+Console.WriteLine(summOfNumber);
+
+int SummOfNumber (int num)
+{
+    int result = 0;
+    for (int i = num; i <= 0; i = i / 10)
+    {
+        result = result + (i / 10);
+    }
+    return result;
+}
