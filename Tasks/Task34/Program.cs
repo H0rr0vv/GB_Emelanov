ac56a1b1e788f6383f186efdee768d668507549e
Horrow <faik2k@gmail.com>
2023-03-12 19:28:13 +1000

    task 34, 36

---
+++ Tasks/Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int value = 0;
+
+
+void FeelArrThreeDigitNumber(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < size - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+
+int EvenNumber (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) value++;
+    }
+    return value;
+}
+
+PrintArray();
+Console.Write(value);
+
---
+++ Tasks/Task36/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Введите массив");
+int[] array = new int[4];
+
+
+void FeelArrThreeDigitNumber(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(0; 100);
+    }
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < size - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+PrintArrayArray();
