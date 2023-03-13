2786f11ceb98d30aa43ae31e44b99b07240fb44f
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-13 13:02:47 +1000

    task 37

--- Tasks/Task34/Program.cs
+++ Tasks/Task34/Program.cs
@@ -9,34 +9,34 @@ int[] array = new int[size];
 int value = 0;
 
 
-void FeelArrThreeDigitNumber(int[] arr)
+void CreateArray(int size, int min, int max)
 {
+    int[] arr = new int[size];
     Random rnd = new Random();
+
     for (int i = 0; i < size; i++)
     {
-        arr[i] = rnd.Next(100, 1000);
+        arr[i] = rnd.Next(min, max + 1);
     }
+    return arr;
 }
-
 
 void PrintArray(int[] arr)
 {
     Console.Write("[");
-
-    
-
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         Console.Write(arr[i]);
-        if (i < size - 1) Console.Write(", ");
+        if (i < arr.Length - 1) Console.Write(", ");
     }
     
     Console.Write("]");
 }
 
 
-int EvenNumber (int[] arr)
+int FindEvenElem (int[] arr)
 {
+    int value = 0;
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i] % 2 == 0) value++;
@@ -44,6 +44,9 @@ int EvenNumber (int[] arr)
     return value;
 }
 
-PrintArray();
-Console.Write(value);
+int[] array = CreateArray(5, 100, 999);
+int valueEvenElem = FindEvenElem(array);
 
+PrintArray(array);
+Console.Write(" -> ");
+Console.Write(valueEvenElem);
--- Tasks/Task36/Program.cs
+++ Tasks/Task36/Program.cs
@@ -4,33 +4,44 @@
 //[-4, -6, 89, 6] -> 0
 
 
-Console.WriteLine("Введите массив");
-int[] array = new int[4];
-
-
-void FeelArrThreeDigitNumber(int[] arr)
+void CreateArray(int size, int min, int max)
 {
+    int[] arr = new int[size];
     Random rnd = new Random();
+
     for (int i = 0; i < size; i++)
     {
-        arr[i] = rnd.Next(0; 100);
+        arr[i] = rnd.Next(min, max + 1);
     }
+    return arr;
 }
-
 
 void PrintArray(int[] arr)
 {
     Console.Write("[");
-
-    
-
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         Console.Write(arr[i]);
-        if (i < size - 1) Console.Write(", ");
+        if (i < arr.Length - 1) Console.Write(", ");
     }
     
     Console.Write("]");
 }
 
-PrintArrayArray();
+int FindSummNotEvenPositionElem (int[] arr)
+{
+    int res = 0;
+    for (i = 1; i < arr.Length; i = i + 2)
+    {
+        res += arr[i];
+    }
+}
+
+int[] array = CreateArray(10, 0, 10);
+PrintArray(array);
+
+int summNotEvenPositionElem = FindSummNotEvenPositionElem(array);
+Console.Write(" -> ");
+Console.Write(summNotEvenPositionElem);
+
+
