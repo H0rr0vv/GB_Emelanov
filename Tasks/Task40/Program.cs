1ee72e4f0225829f7759b78cbc0e4c3549321e7e
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-14 12:02:47 +1000

    task40

---
+++ Tasks/Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 40:
+//Напишите программу, которая принимат на вход три числа и проверяет,
+//может ли существовать треугольник со сторонами такой длины
+//теорема - каждая сторона треугольника меньше суммы двух других сторон
+
+Console.WriteLine("Длина первой стороны = ");
+int ASide = Console.Read();
+Console.WriteLine("Длина второй стороны = ");
+int BSide = Console.Read();
+Console.WriteLine("Длина третьей стороны = ");
+int CSide = Console.Read();
+
+bool CheckTriangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+Console.Write(CheckTriangle(ASide, BSide, CSide) 
+    ? "Треугольник с такими сторонами может существовать" 
+    : "Треугольника с такими сторонами существовать не может");
