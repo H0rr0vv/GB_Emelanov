eb3109cb24a4dd4ada613a018d22d983dfc18b72
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-14 10:50:38 +1000

    task35

--- Tasks/Task35/Program.cs
+++ Tasks/Task35/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿//Задача 35:
+//Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в
+//отрезке [10, 99].
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+int FindTwoDigitElem (int[] arr)
+{
+    int value = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) value++;
+    }
+    return value;
+}
+
+
+int[] array = CreateArray(123, 0, 999);
+int valueTwoDigitElem = FindTwoDigitElem(array);
+
+PrintArray(array);
+Console.Write(" -> ");
+Console.Write(valueTwoDigitElem);
