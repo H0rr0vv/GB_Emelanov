7c56c68984cfd3a53249ab62074363bbaa349409
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-14 10:59:58 +1000

    task38

--- Tasks/Task38/Program.cs
+++ Tasks/Task38/Program.cs
@@ -1,1 +1,66 @@
-﻿
+﻿//Задача 38:
+//Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементами массива.
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        double num = rnd.NextDouble() * ((max - min) + min);
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+double FindMax(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+double FindMin(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+double DiffMaxToMin(double max, double min)
+{
+    double res = Math.Round(max - min, 1);
+    return res;
+}
+
+
+double[] array = CreateArrayRndDouble(4, 0, 10);
+double maxValue = FindMax(array);
+double minValue = FindMin(array);
+double diffMaxToMin = DiffMaxToMin(maxValue, minValue);
+
+PrintArrayDouble(array);
+Console.Write(" -> ");
+Console.Write(diffMaxToMin);
