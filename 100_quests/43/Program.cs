f55f575d33e36536bf7a6b1572fb1ab905434d4a
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-16 11:10:47 +1000

    100 quests

---
+++ 100_quests/43/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 43:
+//Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+int ConvertToBinary (int dig)
+{
+    int binaryDig = 0;
+	int i = 0;
+    while (dig > i)    
+    {
+        binaryDig = binaryDig + (dig % 2);
+        binaryDig = binaryDig * 10;
+		dig = dig / 2;
+    }
+    return binaryDig / 10;
+}
+
+Console.WriteLine ("Введите число в десятичной системе");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int binaryDigit = ConvertToBinary(digit);
+Console.WriteLine(binaryDigit);
---
+++ 100_quests/45/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 45:
+//Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//Первые два числа Фибоначчи: 0 и 1.
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+
+int[] Fibonachi (int n)
+{
+    int[] fib = new int[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите количество первых чисел Фибоначчи");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] fibonachi = Fibonachi(N);
+PrintArray(fibonachi);
