29fbe33e47c9391b5e0b32da56a321adc9961bc0
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-16 14:20:15 +1000

    48

--- 100_quests/48/Program.cs
+++ 100_quests/48/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿//48. Показать двумерный массив размером m×n 
+//заполненный целыми числами
+
+int[,] CreateArray (int m, int n, int min, int max)
+{
+    int[,] arr = new int [m,n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           arr[m, n] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray (int[,] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j]);
+        }
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+
+int[,] array = CreateArray(3, 4, 0, 10);
+PrintArray(array);
