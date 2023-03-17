e019712959bb1a509bea9c046403c5aae849a876
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-17 12:04:01 +1000

    task47

--- Tasks/Task49/Program.cs
+++ Tasks/Task49/Program.cs
@@ -1,37 +1,14 @@
-﻿//Задача 46:
-//Задайте двумерный массив размером mxn,
-//заполненный случайными целыми числами
-//  1   4   8   19
-//  5   -2  33  -2
-//  77  3   8   1
+﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, у 
+которых оба индекса чётные, и замените эти элементы на их 
+квадраты.
+Например, изначально массив
+ выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть 
+вот так:
+ 1 4 49 2
+ 5 9 2 3
+64 4 4 4*/
 
-
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
-    int[,] matrix = new int[rows,columns];
-    Random rnd = new Random();
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = Next.rnd(min, max + 1);
-        }
-    }
-    return matrix; 
-}
-
-void PrintMatrix (int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write("${matrix[i, j], 4}");
-        }
-        Console.WriteLine("");
-    }
-}
-
-int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
-PrintMatrix(array2d);
--- Tasks/Task51/Program.cs
+++ Tasks/Task51/Program.cs
@@ -1,37 +1,9 @@
-﻿//Задача 46:
-//Задайте двумерный массив размером mxn,
-//заполненный случайными целыми числами
-//  1   4   8   19
-//  5   -2  33  -2
-//  77  3   8   1
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму 
+элементов, находящихся на главной диагонали (с индексами 
+(0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12*/
 
-
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
-    int[,] matrix = new int[rows,columns];
-    Random rnd = new Random();
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = Next.rnd(min, max + 1);
-        }
-    }
-    return matrix; 
-}
-
-void PrintMatrix (int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write("${matrix[i, j], 4}");
-        }
-        Console.WriteLine("");
-    }
-}
-
-int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
-PrintMatrix(array2d);
