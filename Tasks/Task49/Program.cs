274ff3f3794cf97974485fdccbb3510c078b0d7c
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-17 13:14:28 +1000

    task50

--- Tasks/Task49/Program.cs
+++ Tasks/Task49/Program.cs
@@ -12,3 +12,52 @@
  5 9 2 3
 64 4 4 4*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int [,] MatrixDoubleEvenInd(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j] ;
+            }
+        }
+
+    }
+    return matrix; 
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+MatrixDoubleEvenInd(array2d);
+Console.WriteLine("");
+Console.WriteLine("");
+PrintMatrix(array2d);
--- Tasks/Task50/Program.cs
+++ Tasks/Task50/Program.cs
@@ -9,3 +9,51 @@
 1, 7 -> такого элемента
 в массиве нет*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("${matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void FindPositionMatrix(int[,] matrix, int indRow, int indColumn)
+{
+    if (indRow >= matrix.GetLength(0) 
+        && indColumn >= matrix.GetLength(1))
+    {
+        Console.WriteLine(
+            $"{indRow}, {indColumn} -> {matrix[indRow, indColumn]}");
+    }
+    else
+    {
+        Console.WriteLine(
+            $"{indRow}, {indColumn} -> такого элемента в массиве нет");
+    }
+}
+
+int row = Convert.ToInt32(Console.ReadLine());
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+int findPositionMatrix = FindPositionMatrix(array2d, row, column);
+
