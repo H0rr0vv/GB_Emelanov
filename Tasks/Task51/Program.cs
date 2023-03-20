ad62c7194ef72b621eb4b1a74f673ef10e5881df
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-20 11:38:04 +1000

    task 51

--- Tasks/Task51/Program.cs
+++ Tasks/Task51/Program.cs
@@ -7,3 +7,70 @@
 8 4 2 4
 Сумма элементов главной диагонали: 1+9+2 = 12*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int FindSumDiagonalDigit(int[,] matrix)
+{
+    int diagonalDigitSum = 0;
+    for (int i = 0; i < matrix.GetLength(0) 
+        && i < matrix.GetLength(1); i++)
+    {
+        int j = i;
+        diagonalDigitSum = diagonalDigitSum + matrix[i, j];
+    }
+    return diagonalDigitSum;
+}
+
+void PrintFindSumDiagonalDigit(int[,] matrix, int sum)
+{
+    Console.Write("Сумма элементов главной диагонали: ");
+    for (int i = 0; i < matrix.GetLength(0) 
+        && i < matrix.GetLength(1); i++)
+    {
+        int j = i;
+        Console.Write($"{matrix[i, j]} ");
+        if (i + 1 < matrix.GetLength(0)
+            && i + 1 < matrix.GetLength(1))
+        {
+            Console.Write("+ ");
+        }
+        else
+        {
+            Console.Write($"= {sum}");
+        }
+    }
+}
+
+
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+int diagonalDigitSum = FindSumDiagonalDigit(array2d);
+PrintFindSumDiagonalDigit(array2d, diagonalDigitSum);
