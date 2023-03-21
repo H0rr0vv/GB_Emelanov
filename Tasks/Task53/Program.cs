bf390073478e18b14a4b049ed552d399f0e1330f
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-21 16:24:03 +1000

    task 54

--- Tasks/Task53/Program.cs
+++ Tasks/Task53/Program.cs
@@ -2,3 +2,46 @@
 Задайте двумерный массив. Напишите программу, которая поменяет
 местами первую и последнюю строку массива.*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] ChangeFirstEndRow (int[,] arr)
+{
+    int[] tempArr = new int[arr.GetLength(0)];
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        tempArr[j] = arr[0, j];
+        arr[0, j] = arr[arr.GetLength(0) - 1, j];
+        arr[arr.GetLength(0) - 1, j] = tempArr[j];
+    }
+	return arr;
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+array2d = ChangeFirstEndRow(array2d);
+PrintMatrix(array2d);
--- Tasks/Task54/Program.cs
+++ Tasks/Task54/Program.cs
@@ -1,4 +1,6 @@
-﻿/*Задача 54: Задайте двумерный массив. 
+﻿/*
+Задача 54: 
+Задайте двумерный массив. 
 Напишите программу, которая упорядочит по убыванию элементы 
 каждой строки двумерного массива.
 Например, задан массив:
@@ -10,3 +12,54 @@
 9 5 3 2
 8 4 4 2*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] SortRow (int[,] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 1; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j - 1] < arr[i, j])
+            {
+                temp = arr[i, j - 1];
+                arr[i, j - 1] = arr[i, j];
+                arr[i, j] = temp;
+                j = 0;
+            }
+        }
+    }
+	return arr;
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+array2d = SortRow(array2d);
+Console.WriteLine("");
+PrintMatrix(array2d);
