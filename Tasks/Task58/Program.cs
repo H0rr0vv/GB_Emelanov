6292c9035cb3903ed6656af048f046e194774ba9
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-24 15:28:21 +1000

    task 58

--- Tasks/Task58/Program.cs
+++ Tasks/Task58/Program.cs
@@ -7,3 +7,56 @@
 18 20
 15 18*/
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] ProductTwoMatrix (int[,] matrix1, int[,] matrix2)
+{
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                resultMatrix[i, j] = resultMatrix[i, j] +
+                (matrix1[i, k] * matrix2[k, j]);
+            }
+        }
+    }
+	return resultMatrix;
+}
+
+
+int[,] firstMatrix = CreateMatrixRndInt(2, 2, 0, 5);
+int[,] secondMatrix = CreateMatrixRndInt(2, 2, 0, 5);
+int[,] resultMatrix = ProductTwoMatrix(firstMatrix, secondMatrix);
+PrintMatrix(firstMatrix);
+Console.WriteLine("");
+PrintMatrix(secondMatrix);
+Console.WriteLine("____________");
+PrintMatrix(resultMatrix);
