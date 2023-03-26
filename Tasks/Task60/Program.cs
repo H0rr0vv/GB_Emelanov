6dbb423828b00ecc5a5627d364dda33b66f1ea9e
Horrow <faik2k@gmail.com>
2023-03-26 18:27:18 +1000

    task 60

--- Tasks/Task60/Program.cs
+++ Tasks/Task60/Program.cs
@@ -7,3 +7,41 @@
 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)*/
 
+int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write()
+            }
+        }
+    }
+}
+
+int[,,] matrix = CreateMatrixRndInt(3, 4, 5, 0, 100);
+PrintMatrix(matrix);
+Console.WriteLine("");
+PrintMatrix(secondMatrix);
+Console.WriteLine("____________");
+PrintMatrix(resultMatrix);
