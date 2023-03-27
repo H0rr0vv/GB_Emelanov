1f9a26d076c3e6cff2d40d0b214d3b8becc66597
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-03-27 11:27:33 +1000

    task 69

--- Tasks/Task69/Program.cs
+++ Tasks/Task69/Program.cs
@@ -7,3 +7,16 @@
 А = 2; В = 3 -> 8
 */
 
+int NumberAToNumberB(int a, int b)
+{
+    if (b == 1) return a;
+    return a * NumberAToNumberB(a, b - 1);
+}
+
+Console.WriteLine("Введите первое число");
+int numberA = 3;
+//Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = 5;
+//Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A = {numberA}; B = {numberB} -> {NumberAToNumberB(numberA, numberB)}");
