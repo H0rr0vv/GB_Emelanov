4a090855f7891989c250ffd9da4c4f3a62610109
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-06-08 09:57:16 +1000

    tasks 20 (re) 25, 27

--- Python/Task_20.py
+++ Python/Task_20.py
@@ -27,7 +27,7 @@ Q, Z – 10 очков.
     12
 '''
 
-my_word = input(f'Слово: ')
+my_word = input(f'Слово: ').lower()
 points = int(0)
 
 en_ru_dict = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 's': 1, 't': 1, 'r': 1, 
