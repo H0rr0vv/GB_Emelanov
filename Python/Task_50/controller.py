ba4ae001193f12a2f0448f748d578f4008a31d71
Емельянов Богдан Андреевич <b.emelyanov@szakaz27.ru>
2023-06-15 11:01:21 +1000

    Task 50

---
+++ Python/Task_50/controller.py
@@ -0,0 +1,22 @@
+from view import menu
+
+def start():
+    while True:
+        choice = menu()
+        match choice:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                break
---
+++ Python/Task_50/view/__init__.py
@@ -0,0 +1,4 @@
+from .text import main_menu, menu_choice, input_error
+from .console import menu
+
+__all__ = ['main_menu', 'menu']
