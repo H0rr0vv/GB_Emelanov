a8df2453157882f7b438e0a8fb35d7bb8a1c81f3
Horrow <faik2k@gmail.com>
2023-07-02 16:01:04 +1000

    java hm 1 task 2

--- Java/Homework_1/Task_1/Main.java
+++ Java/Homework_1/Task_1/Main.java
@@ -1,6 +1,16 @@
 package Java.Homework_1.Task_1;
+import java.util.Scanner;
 
 public class Main {
     /* Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n) */
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ввелите число: ");
+        int value = in.nextInt();
+        int res = 1;
+        for (int i = 2; i <= value; i++) {
+            res = res + i;
+        }
+        System.out.printf("Искомое треугольное число равно: %d", res);
+    }
 }
--- Java/Homework_1/Task_2/Main.java
+++ Java/Homework_1/Task_2/Main.java
@@ -2,4 +2,31 @@ package Java.Homework_1.Task_2;
 
 public class Main {
     /* Вывести все простые числа от 1 до 1000 */
+    public static void main(String[] args) {
+        System.out.println("Программа выводит все простые числа от 1 до 1000");
+        int value = 1000;
+        int smaller = 0;
+        int nod = 1;
+        int accuracy = 2;
+        for (int i = 2; i <= value; i++) {
+            for (int k = 1; k <= accuracy; k++) {
+                int temp = (int) (Math.random() * ((i - 1) - 2 + 1) + 2);
+                if (i > temp) {
+                    smaller = temp;
+                } else {
+                    smaller = i;
+                }
+                for (int j = 2; j <= smaller; j++) {
+                    if (i % j == 0 && temp % j == 0) {
+                        nod = j;
+                    }
+                }
+                if (nod == 1 && Math.pow(nod, (i - 1) / 2) != nod / i) {
+                    System.out.println("i");                   
+                }
+                Math.pow(i, 2)
+                System.out.println(i);
+            }
+        }
+    }
 }
