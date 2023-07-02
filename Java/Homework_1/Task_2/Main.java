b1b23959c817ec76d3d218ba97c119001b207aac
Horrow <faik2k@gmail.com>
2023-07-02 19:30:59 +1000

    fff]

--- Java/Homework_1/Task_2/Main.java
+++ Java/Homework_1/Task_2/Main.java
@@ -8,23 +8,30 @@ public class Main {
         int smaller = 0;
         int nod = 1;
         int accuracy = 2;
+        int res = 0;
+        float degree = 0;
         for (int i = 2; i <= value; i++) {
             for (int k = 1; k <= accuracy; k++) {
-                int temp = (int) (Math.random() * ((i - 1) - 2 + 1) + 2);
+                int temp = (int) (Math.random() * ((100 - 1) - 2 + 1) + 2);
                 if (i > temp) {
                     smaller = temp;
                 } else {
                     smaller = i;
                 }
+                nod = 1;
                 for (int j = 2; j <= smaller; j++) {
                     if (i % j == 0 && temp % j == 0) {
                         nod = j;
                     }
                 }
-                if (nod == 1 && Math.pow(nod, (i - 1) / 2) != nod / i) {
-                    System.out.println("i");                   
+                degree = (i - 1) / 2;
+                res = 1;
+                for (int l = 1; l < degree; l++) {
+                    res = res * nod;
                 }
-                Math.pow(i, 2)
+            }
+
+            if (nod == 1 && res != nod / i) {
                 System.out.println(i);
             }
         }
--- Java/Homework_1/Task_3/Main.java
+++ Java/Homework_1/Task_3/Main.java
@@ -1,5 +1,39 @@
 package Java.Homework_1.Task_3;
 
+import java.util.Scanner;
+
 public class Main {
     /* Реализовать простой калькулятор */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int value_1 = 0;
+        int value_2 = 0;
+        int res = 0;
+        String sight = "";
+        System.out.println("Введите первое число: ");
+        value_1 = in.nextInt();
+        System.out.println("Введите действие: ");
+        sight = in.next();        
+        System.out.println("Введите второе число: ");
+        value_2 = in.nextInt();
+        if (sight == "+") {
+            res = value_1 + value_2;
+            System.out.println(res);
+        }
+        else if (sight == "-") {
+            res = value_1 - value_2;
+            System.out.println(res);
+        }
+        else if (sight == "*") {
+            res = value_1 * value_2;
+            System.out.println(res);
+        }
+        else if (sight == "/") {
+            res = value_1 / value_2;
+            System.out.println(res);
+        }
+        else {
+            System.out.println("Ошибка");
+        }        
+    }
 }
